09ff447854658448f82adb57e63fecc1537a27af
Abhinav Bharadwaj <bharadwajsharma1997@gmail.com>
2021-06-13 12:01:48 +0530

    Pattern Printing using Java

---
+++ PatternPrintingExamples/RightTrinagle.java
@@ -0,0 +1,29 @@
+package PatternPrintingExamples;
+
+import java.util.*;
+
+public class RightTrinagle {
+
+    public static void rightTriangle(int n) 
+    { 
+        int i, j;  
+        for(i=0; i<n; i++)                      //outer loop to print number of rows(n)
+        {
+            System.out.print(" ");              // line for printing space
+            for(j=0; j<=i; ++j) {               // inner loop to print columns
+                System.out.print("* ");         // print symbol
+            }           
+            System.out.println();               // ending line after each row
+        } 
+    } 
+    public static void main(String args[]) 
+    { 
+        int rows;
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nEnter number of Rows : ");
+        rows = input.nextInt();
+        rightTriangle(rows);
+        input.close(); 
+    } 
+    
+}
